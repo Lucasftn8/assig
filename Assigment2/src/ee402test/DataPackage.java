19f0001a752806b93aebe15531a6979abfb07cde
Lucasftn8 <47553598+Lucasftn8@users.noreply.github.com>
2019-12-13 01:38:40 +0000

    Add files via upload

---
+++ Assigment2/src/ee402test/DataPackage.java
@@ -0,0 +1,20 @@
+package ee402test;
+
+public class DataPackage {
+	   //Same as Client DataPackage
+	   private float t_temp;
+		private String  t_time;// value of reference
+		
+		public DataPackage(float _temp,String _time) {
+			this.t_temp=_temp;
+			this.t_time=_time;
+			//receive here the thread server value?
+		}
+		public DataPackage getDataPackage() {
+			t_times
+			return this;
+		}
+		//GETTER
+		public float get_temp() {return t_temp;}
+		public String get_time() {return t_time;}
+}
